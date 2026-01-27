0702f7218bc898c6fb2bf33acb69e3126fdd0c38
Siddharth Patankar <siddharthpatankar4@gmail.com>
2026-01-27 14:49:59 +0530

    feat:added file ffrom vs code

---
+++ visual_sode_temp.py
@@ -0,0 +1,12 @@
+#This line is spoken after Might Guy opens the Eighth Gate (Gate of Death) and nearly kills Madara—making it one of the most iconic moments in anime history.
+#It’s powerful because Madara never acknowledges anyone else as his equal or superior in taijutsu.
+
+#If you want:
+
+#the full scene explanation,
+
+#a motivational rewrite, or
+
+#this quote in Marathi / Hindi / stylish poster format,
+
+#just tell me 👍
